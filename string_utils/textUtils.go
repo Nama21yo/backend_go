84150c95769573338b968f22f18f02487a00ab8d
Nama21yo <mataniayohanes@gmail.com>
2025-07-14 16:37:46 +0300

    feat: add palindrome check with normalization of input

--- string_utils/textUtils.go
+++ string_utils/textUtils.go
@@ -26,4 +26,19 @@ func WordFrequency(text string) map[string]int {
 	return wordFreq
 }
 
+// IsPalindrome returns true if the input string is a palindrome (ignores case, spaces, punctuation).
+func IsPalindrome(s string) bool {
+	var filtered []rune
+	for _, char := range s {
+		if unicode.IsLetter(char) || unicode.IsDigit(char) {
+			filtered = append(filtered, unicode.ToLower(char))
+		}
+	}
 
+	for i, j := 0, len(filtered)-1; i < j; i, j = i+1, j-1 {
+		if filtered[i] != filtered[j] {
+			return false
+		}
+	}
+	return true
+}
