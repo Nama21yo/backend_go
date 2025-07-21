b08fa13ecdd98de846cab0868a584df747c64db4
Nama21yo <mataniayohanes@gmail.com>
2025-07-21 05:44:10 +0300

    feat(controller): add handler functions for CRUD task endpoints

---
+++ task_manager/controllers/task_controller.go
@@ -0,0 +1,74 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+	"task_manager/data"
+	"task_manager/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetTasks(c *gin.Context) {
+	c.JSON(http.StatusOK, data.GetAllTasks())
+}
+
+func GetTask(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid ID"})
+		return
+	}
+	task, err := data.GetTaskByID(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, task)
+}
+
+func CreateTask(c *gin.Context) {
+	var newTask models.Task
+	if err := c.ShouldBindJSON(&newTask); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if newTask.DueDate.IsZero() {
+		newTask.DueDate = time.Now().Add(24 * time.Hour)
+	}
+	created := data.CreateTask(newTask)
+	c.JSON(http.StatusCreated, created)
+}
+
+func UpdateTask(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid ID"})
+		return
+	}
+	var updatedTask models.Task
+	if err := c.ShouldBindJSON(&updatedTask); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	task, err := data.UpdateTask(id, updatedTask)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, task)
+}
+
+func DeleteTask(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid ID"})
+		return
+	}
+	if err := data.DeleteTask(id); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.Status(http.StatusNoContent)
+}
